d830c3d5fa8aaeac8d2fec8b866634e55e67f0ff
Stephany Resque <resquestephany@gmail.com>
2024-09-24 09:35:30 -0300

    Create modelo.py

---
+++ revisao/modelo.py
@@ -0,0 +1,11 @@
+from abc import ABC
+
+class Professor:
+
+    def __init__(self, nome, nTurmas, salario):
+        self.nome = nome
+        self.nTurmas = nTurmas
+        self.salario = salario
+
+    
+
